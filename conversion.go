0157a79a94e396f897df0f4a197528b7a366c2fb
Matthew Elliston <mpgelliston@gmail.com>
2020-10-05 22:25:51 +0100

    Added Kelvin scale and fixed some output

--- conversion.go
+++ conversion.go
@@ -8,6 +8,9 @@ type Celcius float64
 // Fahrenheit Temperature Type
 type Fahrenheit float64
 
+// Kelvin Temperature Type
+type Kelvin float64
+
 const (
 	// AbsoluteZeroC The point of absolute zero in celcius
 	AbsoluteZeroC Celcius = -273.15
@@ -15,10 +18,13 @@ const (
 	FreezingC Celcius = 0
 	// BoilingC the temperature in Celcius that water boils.
 	BoilingC Celcius = 100
+	//AbsoluteZeroK The point of absolute zero in Kelvin
+	AbsoluteZeroK Kelvin = 0
 )
 
-func (c Celcius) String() string    { return fmt.Sprintf("%g•C", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g•F", f) }
+func (c Celcius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
 
 // CToF Convert Celcius to Fahrenheit
 func CToF(c Celcius) Fahrenheit {
@@ -28,4 +34,14 @@ func CToF(c Celcius) Fahrenheit {
 // FToC Convert Fahrenheit to Celcius
 func FToC(f Fahrenheit) Celcius {
 	return Celcius((f - 32) * 5 / 9)
+}
+
+// CToK Convert Celcius To Kelvin
+func CToK(c Celcius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
+// KToC Convert Kelvin To Celcius
+func KToC(k Kelvin) Celcius {
+	return Celcius(k - 273.15)
 }
